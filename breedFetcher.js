add173bcbf0804b7a2dab4d01f1195911349e438
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-18 23:41:18 +0000

    requested from search query and inline agrv

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,4 +1,18 @@
 const request = require('request');
+const agrv = process.argv.slice(2);
+const breed = agrv[0];
+const website = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
+request(website, (error, body) => {
+  let data = JSON.parse(body.body);
 
- console.log(request)
+  if (error) {
+    return undefined;
+  }
+
+  if (data.length === 0) {
+    console.log("Breed is not found");
+  } else {
+    console.log(data[0].description);
+  }
+});
