ccf199ad51b100d360f9fe597c766be206f4f7ae
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-19 00:45:46 +0000

    refactored, mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,17 @@
 const request = require('request');
-const agrv = process.argv.slice(2);
-const breed = agrv[0];
-const website = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(website, (error, body) => {
-  let data = JSON.parse(body.body);
+const fetchBreedDescription = function(breedName, callback) {
+  const website = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(website, (error, body) => {
+    let data = JSON.parse(body.body);
+    
+    if (data.length === 0) {
+      callback(error, null);
+    } else {
+      callback(null, data[0].description.trim());
+    }
+  });
+};
 
-  if (error) {
-    return undefined;
-  }
 
-  if (data.length === 0) {
-    console.log("Breed is not found");
-  } else {
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription };
